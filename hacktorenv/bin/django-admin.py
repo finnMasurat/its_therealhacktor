fc6a257461c2bee1c0ac38fa7a5ca8fed558842f
Finn Masurat <finn.masurat@haw-hamburg.de>
2016-12-16 15:13:00 +0000

    removed pyc files

--- hacktorenv/bin/django-admin.py
+++
@@ -1,4 +0,0 @@
-#!/var/www/theRealHacktorUnchained/hacktorenv/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.10.4','django-admin.py'
-__requires__ = 'Django==1.10.4'
-__import__('pkg_resources').run_script('Django==1.10.4', 'django-admin.py')
