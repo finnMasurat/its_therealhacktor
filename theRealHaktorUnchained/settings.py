ce814b827a84a31dfeb034b3ad7a63a1c976897b
Finn Masurat <finn.masurat@haw-hamburg.de>
2016-12-20 12:48:14 +0000

    html und settings update

--- theRealHaktorUnchained/settings.py
+++ theRealHaktorUnchained/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wbkz@=i5k7m1o!3pb!@2w^@ms+ptlu+hz0qc7#&+7=i91bp%gn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.18.0.14']
+ALLOWED_HOSTS = ['172.18.0.12','theRealHacktor.informatik.haw-hamburg.de']
 
 
 # Application definition
